ad591a979032ae769fcb0585e3829c0f10ca001c
Pathi Anil <madhuanil25@gmail.com>
2024-01-20 00:02:34 +0530

    variables

---
+++ 04-sum.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($1 + $2))
+
+echo $SUM
---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+echo "Enter your name"
+read -s name
+#echo "Your name is: $name"
+
+
+echo "Enter your password"
+read -s password
+#echo "Your name is: $password"
