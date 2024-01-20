5f860a6938b728e79fc42df71e09c419350ec6ce
Pathi Anil <madhuanil25@gmail.com>
2024-01-20 23:49:39 +0530

    cond

--- 08-install.sh
+++ 08-install.sh
@@ -2,12 +2,12 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "Run the script with root user"
     exit 1
 else 
-    ehco "you are eroot user"
+    echo "you are root user"
 fi
 
 yum install git -y
